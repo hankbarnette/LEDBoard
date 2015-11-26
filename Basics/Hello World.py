634d1fc738003bca53ef425f45411ba18191b3e5
hankbarnette <henrybernet@gmail.com>
2015-11-26 03:08:52 +0000

    Initial Commit

---
+++ Basics/Hello World.py
@@ -0,0 +1,18 @@
+print('hello')
+
+number = 100
+
+
+
+
+def penismouth( dicks ):    
+    for dickfer in range(dicks):
+        print(dickfer)
+        if dickfer % 10 == 0:
+            print('penis mouth')
+    return
+
+penismouth(100)
+            
+
+    
